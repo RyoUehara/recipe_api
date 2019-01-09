eb9adceb1122c62d1b37fcd95f23809a89bab281
Unknown <mukudori88888@gmail.com>
2019-01-09 20:37:40 +0900

    unuse dotenv

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-require('dotenv').config();
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : process.env.DATABASE_HOST,
